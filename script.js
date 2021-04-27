d60c1e9a02a8fb5686ac8138eba46247b6d61f9d
JillBirchall <jill.m.birchall@gmail.com>
2021-04-27 14:48:35 +0100

    All difficulty levels added. Bugs fixed.

--- script.js
+++ script.js
@@ -4,226 +4,281 @@ const gameContainer = document.getElementById("gameContainer");
 const squaresDisplay = document.querySelectorAll("[data-square]");
 const gameInfoDisplay = document.getElementById("gameInfo");
 const newGameBtn = document.getElementById("newGameBtn");
-let takenSquares = []; //Stores a list of all the squares that contain pieces
+let grid = []; //Stores the current board set-up
 let isTwoPlayer = false;
 let difficulty = "";
 let currentPlayer = 1;
 let gameInProgress = false;
 let isPlayerTurn = true;
 
-gameOption.forEach(option => {
-    option.addEventListener("click", e => {
-        let gameOption = e.target.dataset.gameOption;
-        setGameType(gameOption);
-        startGame();
-    })
+gameOption.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    let gameOption = e.target.dataset.gameOption;
+    setGameType(gameOption);
+    startGame();
+  });
 });
 
 squaresDisplay.forEach((item) => {
-    item.addEventListener("click", (e) => {
-        if (isSquareEmpty(e.target.dataset.square) && gameInProgress && playerTurn) {
-            nextTurn(e);
-        }
-    });
+  item.addEventListener("click", (e) => {
+    if (
+      isSquareEmpty(e.target.dataset.square) &&
+      gameInProgress &&
+      isPlayerTurn
+    ) {
+      nextTurn(e);
+    }
   });
+});
 
-  newGameBtn.addEventListener("click", () => {
-    resetGame();
-  });
-
+newGameBtn.addEventListener("click", () => {
+  resetGame();
+});
 
 function setGameType(option) {
-    if (option === "2-player") {
-        isTwoPlayer = true;
-    } else {
-        isTwoPlayer = false;
-        difficulty = option;
-    }
+  if (option === "2-player") {
+    isTwoPlayer = true;
+  } else {
+    isTwoPlayer = false;
+    difficulty = option;
+  }
 }
 
- function isSquareEmpty(square) {
-    return takenSquares.indexOf(square) === -1 ? true : false;
- } 
+function isSquareEmpty(square) {
+  return grid[square] === "EMPTY" ? true : false;
+}
 
 function startGame() {
-    gameOptionsList.classList.add("hidden");
-    gameContainer.classList.remove("hidden");
-    gameInProgress = true;
-    isTwoPlayer ? updateTurnInfo("Player 1") : updateTurnInfo("Player");
-  
+  initialiseBoard();
+  gameOptionsList.classList.add("hidden");
+  gameContainer.classList.remove("hidden");
+  gameInProgress = true;
+  isTwoPlayer ? updateTurnInfo("Player 1") : updateTurnInfo("Player");
+}
+
+function initialiseBoard() {
+  for (let i = 0; i < 9; i++) {
+    grid.push("EMPTY");
+  }
 }
 
 function nextTurn(event) {
-    if (isTwoPlayer) {
-        playerTurn(event.target);
-        currentPlayer === 1? currentPlayer = 2 : currentPlayer = 1; //Swap Player
-    } else {
-        playerTurn(event.target);
-        currentPlayer = 2;
-        isPlayerTurn = false;
-        setTimeout(computerTurn, 1000);
+  if (isTwoPlayer) {
+    playerTurn(event.target);
+    currentPlayer === 1 ? (currentPlayer = 2) : (currentPlayer = 1); //Swap Player
+  } else {
+    playerTurn(event.target);
+    if (gameInProgress) {
+      currentPlayer = 2;
+      isPlayerTurn = false;
+      setTimeout(computerTurn, 1000);
     }
+  }
 }
 
 function playerTurn(squareClicked) {
-    if (currentPlayer === 1) {
-        makeMove("X", squareClicked);
-        checkWin("X");
-        if (gameInProgress) isTwoPlayer ? updateTurnInfo("Player 2") : updateTurnInfo("Computer");
+  let currentPiece;
+
+  currentPlayer === 1 ? (currentPiece = "X") : (currentPiece = "O");
+
+  makeMove(currentPiece, squareClicked);
+
+  if (checkWin(currentPiece, grid)) {
+    console.log("Player Wins!");
+    endGame(currentPiece);
+  } else if (checkDraw()) {
+    console.log("Draw");
+    endGame("DRAW");
+  } else {
+    if (isTwoPlayer) {
+      currentPlayer === 1
+        ? updateTurnInfo("Player 2")
+        : updateTurnInfo("Player 1");
     } else {
-        isPlayerTurn = false;
-        makeMove("O", squareClicked);
-        checkWin("O");
-        if (gameInProgress) isTwoPlayer ? updateTurnInfo("Player 1") : updateTurnInfo("Player");
+      isPlayerTurn = false;
+      updateTurnInfo("Computer");
     }
+  }
 }
 
 function computerTurn() {
-    makeEasyComputerMove();
-    checkWin("O");
-    if (gameInProgress) {
-        isPlayerTurn = true;
-        currentPlayer = 1;
-        updateTurnInfo("Player");
-    }
+  console.log("Computer Turn");
+  switch (difficulty) {
+    case "easy":
+      makeEasyComputerMove();
+      break;
+    case "medium":
+      makeMediumComputerMove();
+      break;
+    case "hard":
+      makeHardComputerMove();
+      break;
+    default:
+      makeEasyComputerMove();
+  }
+
+  if (checkWin("O", grid)) {
+    endGame("O");
+  } else if (checkDraw()) {
+    endGame("DRAW");
+  } else {
+    isPlayerTurn = true;
+    currentPlayer = 1;
+    updateTurnInfo("Player");
+  }
+}
+
+function makeRandomMove() {
+  if (grid.includes("EMPTY") && gameInProgress) {
+    let randomSquare;
+
+    do {
+      randomSquare = String(Math.floor(Math.random() * 9));
+    } while (!isSquareEmpty(randomSquare));
+
+    let square = document.querySelector(`[data-square="${randomSquare}"]`);
+    makeMove("O", square);
+  }
 }
 
 function makeEasyComputerMove() {
-
-    if (takenSquares.length < 9 && gameInProgress) {
-        let randomSquare;
-    
-        do {
-          randomSquare = String(Math.floor(Math.random() * 9));
-        } while (!isSquareEmpty(randomSquare));
-    
-        let square = document.querySelector(`[data-square="${randomSquare}"]`);
-        makeMove("O", square);
+  makeRandomMove();
 }
+
+function makeMediumComputerMove() {
+  for (let i = 0; i < 9; i++) {
+    if (isSquareEmpty(i)) {
+      if (isWinningMove(i, "O")) {
+        makeMove("O", document.querySelector(`[data-square="${i}"]`));
+        return;
+      }
+    }
+  }
+  makeRandomMove();
+}
+
+function makeHardComputerMove() {
+  for (let i = 0; i < 9; i++) {
+    if (isSquareEmpty(i)) {
+      if (isWinningMove(i, "O")) {
+        makeMove("O", document.querySelector(`[data-square="${i}"]`));
+        return;
+      }
+    }
+  }
+
+  for (let i = 0; i < 9; i++) {
+    if (isSquareEmpty(i)) {
+      if (isWinningMove(i, "X")) {
+        makeMove("O", document.querySelector(`[data-square="${i}"]`));
+        return;
+      }
+    }
+  }
+
+  makeRandomMove();
+}
+
+function isWinningMove(square, piece) {
+  let tempgrid = [...grid];
+  tempgrid[square] = piece;
+  return checkWin(piece, tempgrid);
 }
 
 function makeMove(piece, squareClicked) {
-    squareClicked.classList.add(piece);
-    takenSquares.push(squareClicked.dataset.square);
+  squareClicked.classList.add(piece);
+  grid[squareClicked.dataset.square] = piece;
 }
 
-function checkWin(piece) {
-    //Horizontal
+//Checks if the game board contains a winning line for the specified piece
+function checkWin(piece, board) {
+  //Horizontal Row
   for (let i = 0; i < 9; i += 3) {
     if (
-      document
-        .querySelector(`[data-square="${i}"]`)
-        .classList.contains(piece) &&
-      document
-        .querySelector(`[data-square="${i + 1}"]`)
-        .classList.contains(piece) &&
-      document
-        .querySelector(`[data-square="${i + 2}"]`)
-        .classList.contains(piece)
+      board[i] === piece &&
+      board[i + 1] === piece &&
+      board[i + 2] === piece
     ) {
-        endGame(piece);
-      return;
+      return true;
     }
   }
-  //Vertical
+
+  //Vertical Columns
   for (let i = 0; i < 3; i++) {
-    if (
-      document
-        .querySelector(`[data-square="${i}"]`)
-        .classList.contains(piece) &&
-      document
-        .querySelector(`[data-square="${i + 3}"]`)
-        .classList.contains(piece) &&
-      document
-        .querySelector(`[data-square="${i + 6}"]`)
-        .classList.contains(piece)
-    ) {
-        endGame(piece);
-      return;
+    if (board[i] == piece && board[i + 3] === piece && board[i + 6] === piece) {
+      return true;
     }
   }
-  //diagonals
+
+  //Diagonal Lines
   if (
-    (document.querySelector(`[data-square="0"]`).classList.contains(piece) &&
-      document.querySelector(`[data-square="4"]`).classList.contains(piece) &&
-      document.querySelector(`[data-square="8"]`).classList.contains(piece)) ||
-    (document.querySelector(`[data-square="2"]`).classList.contains(piece) &&
-      document.querySelector(`[data-square="4"]`).classList.contains(piece) &&
-      document.querySelector(`[data-square="6"]`).classList.contains(piece))
+    (board[0] === piece && board[4] === piece && board[8] === piece) ||
+    (board[2] === piece && board[4] === piece && board[6] === piece)
   ) {
-    endGame(piece);
-    return;
+    return true;
   }
-  //Check for draw
-  if (takenSquares.length === 9) {
-    endGame("DRAW");
+
+  return false;
+}
+
+function checkDraw() {
+  if (grid.includes("EMPTY")) {
+    return false;
   }
+
+  return true;
 }
 
 function updateTurnInfo(player) {
-    gameInfoDisplay.innerText = `${player} Turn`
+  gameInfoDisplay.innerText = `${player} Turn`;
 }
 
 function endGame(winner) {
-    gameInProgress = false;
-    winner === "DRAW" ? displayDraw() : displayWinner();
-    newGameBtn.classList.remove("hidden");
-
+  gameInProgress = false;
+  winner === "DRAW" ? displayDraw() : displayWinner();
+  newGameBtn.classList.remove("hidden");
 }
 
 function displayDraw() {
-    gameInfoDisplay.innerText = "It's a Draw!";
+  gameInfoDisplay.innerText = "It's a Draw!";
 }
 
 function displayWinner() {
-    let winnerText = "";
+  let winnerText = "";
   if (isTwoPlayer) {
-      if (currentPlayer === 1) {
-        winnerText = "Player 1"
-      } else {
-        winnerText = "Player 2"
-      }
+    if (currentPlayer === 1) {
+      winnerText = "Player 1";
+    } else {
+      winnerText = "Player 2";
+    }
   } else {
     if (currentPlayer === 1) {
-        winnerText = "Player"
-      } else {
-        winnerText = "Computer"
-      }
+      winnerText = "Player";
+    } else {
+      winnerText = "Computer";
+    }
   }
-  gameInfoDisplay.innerText = `${winnerText} Wins!`
-
+  gameInfoDisplay.innerText = `${winnerText} Wins!`;
 }
 
 function resetGame() {
-    clearBoard();
-    displayGameOptions();
-    takenSquares = []; 
-    currentPlayer = 1;
-    gameInProgress = false;
-    isPlayerTurn = true;
-
+  clearBoard();
+  displayGameOptions();
+  grid = [];
+  currentPlayer = 1;
+  gameInProgress = false;
+  isPlayerTurn = true;
 }
 
 function displayGameOptions() {
-    gameContainer.classList.add("hidden");
-    newGameBtn.classList.add("hidden");
-    gameOptionsList.classList.remove("hidden");
+  gameContainer.classList.add("hidden");
+  newGameBtn.classList.add("hidden");
+  gameOptionsList.classList.remove("hidden");
 }
 
 function clearBoard() {
-
-    squaresDisplay.forEach((item) => {
-        item.classList.remove("X");
-        item.classList.remove("O");
-      });
+  squaresDisplay.forEach((item) => {
+    item.classList.remove("X");
+    item.classList.remove("O");
+  });
 }
-
-
-
-
-
-
-
-
-
-
